dc423a91f4357f3b7f27366d202218c56db4048b
Mahnoor Siddiqui <57288492+mehnoorsiddiqui@users.noreply.github.com>
2021-09-26 20:26:36 +0500

    local storage added

--- indexES6.js
+++ indexES6.js
@@ -1,46 +1,53 @@
 class Book {
-    constructor(name, author, type) {
-        this.name = name;
-        this.author = author;
-        this.type = type;
-    }
+  constructor(name, author, type) {
+    this.name = name;
+    this.author = author;
+    this.type = type;
+  }
 }
+
 class Display {
-    add(book) {
-        console.log("ading to ui");
-        let tableBody = document.getElementById("tableBody");
-        let uiString = `
-                 <tr>
-                    <td>${book.name}</td>
-                    <td>${book.author}</td>
-                    <td>${book.type}</td>
-                  </tr>
-    
-                     `;
-        tableBody.innerHTML += uiString;
+  //we will pass an object to add method
+  add(book) {
+    let tableBody = document.getElementById("tableBody");
+    let uiString = `
+             <tr>
+                <td>${book.name}</td>
+                <td>${book.author}</td>
+                <td>${book.type}</td>
+                <td>
+                    <a href="#" class="delete-item">
+                        <i class="fas fa-times"></i>
+                    </a>
+                </td>
+            </tr>
+            `;
+    tableBody.innerHTML += uiString;
+  }
+  //implement the clear function
+  clear() {
+    let libraryForm = document.getElementById("libraryForm");
+    libraryForm.reset();
+  }
+
+  //implement the validate function
+  validate(book) {
+    if (book.name.length < 2 || book.author.length < 2) {
+      return false;
+    } else {
+      return true;
     }
-    clear() {
-        let libraryForm = document.getElementById("libraryForm")
-        libraryForm.reset();
+  }
+
+  show(type, showmessage) {
+    let message = document.getElementById("message");
+    let boldtext;
+    if (type == "success") {
+      boldtext = "Success!";
+    } else {
+      boldtext = "Error :(";
     }
-    validate(book) {
-        if (book.name.length < 2 || book.author.length < 2) {
-            return false
-        }
-        else {
-            return true;
-        }
-    }
-    show(type, showmessage) {
-        let message = document.getElementById("message");
-        let boldtext;
-        if (type == "success") {
-            boldtext = "Success!";
-        }
-        else {
-            boldtext = "Error :(";
-        }
-        message.innerHTML = `
+    message.innerHTML = `
                    <div class="alert alert-${type} alert-dismissible fade show" role="alert">
                        <strong>${boldtext}</strong>${showmessage}
                       <button type="button" class="close" data-dismiss="alert" aria-label="Close">
@@ -48,51 +55,97 @@ class Display {
                       </button>
                    </div>
         `;
-        // to remove the header popup after two seconds
-        setTimeout(function () {
-            message.innerHTML = "";
-        }, 2000);
+    setTimeout(function () {
+      message.innerHTML = " ";
+    }, 3500);
+  }
 
+  deleteBook(target) {
+    if (target.parentElement.classList.contains("delete-item")) {
+      target.parentElement.parentElement.parentElement.remove();
     }
+  }
 }
+//Local storage Class
+class Storage {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem("books") === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem("books"));
+    }
+    return books;
+  }
+  static displayBooks() {
+    const books = Storage.getBooks();
+    books.forEach(function (book) {
+      let displays = new Display();
+      displays.add(book);
+    });
+  }
+  static addBooks(book) {
+    const books = Storage.getBooks();
+    books.push(book);
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+  static removeBooks(bookname) {
+    console.log(bookname);
+    const books = Storage.getBooks();
+    books.forEach(function (book, index) {
+      if (book.name === bookname) {
+        books.splice(index,1);
+      }
+    });
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+}
+//DOM load event
+
+document.addEventListener("DOMContentLoaded", Storage.displayBooks);
+
 //Add submit event listener
 
-let libraryForm = document.getElementById("libraryForm")
-libraryForm.addEventListener('submit', libraryFormSubmit);
-
+let libraryForm = document.getElementById("libraryForm");
+libraryForm.addEventListener("submit", libraryFormSubmit);
 function libraryFormSubmit(e) {
+  console.log("YOu have submitted library form");
+  let name = document.getElementById("bookName").value;
+  let author = document.getElementById("author").value;
+  let type;
+  let fiction = document.getElementById("fiction");
+  let literature = document.getElementById("literature");
+  let pyscology = document.getElementById("pyscology");
 
+  if (fiction.checked) {
+    type = fiction.value;
+  } else if (literature.checked) {
+    type = literature.value;
+  } else if (pyscology.checked) {
+    type = pyscology.value;
+  }
+  let book = new Book(name, author, type);
+  console.log(book);
 
-    console.log('YOu have submitted library form');
-    let name = document.getElementById('bookName').value;
-    let author = document.getElementById('author').value;
-    let type;
-    let fiction = document.getElementById('fiction');
-    let literature = document.getElementById('literature');
-    let pyscology = document.getElementById('pyscology');
+  let display = new Display();
+  if (display.validate(book)) {
+    display.add(book);
+    Storage.addBooks(book); //storing to local storage
+    display.clear();
+    display.show("success", " Your book has been successfully added.");
+  } else {
+    display.show("danger", " Sorry you cannot add this book");
+  }
 
-    if (fiction.checked) {
-        type = fiction.value;
-    }
-    else if (literature.checked) {
-        type = literature.value;
-    }
-    else if (pyscology.checked) {
-        type = pyscology.value;
-    }
-    //objects of Book class
-    let book = new Book(name, author, type);
-    //objects of Display class
-    let display = new Display();
-    if (display.validate(book)) {
-        display.add(book);
-        display.clear();
-        display.show("success", " Your book has been successfully added.");
-    }
-    else {
-        display.show("danger", " Sorry you cannot add this book");
-    }
-    e.preventDefault();
-
-
+  console.log(book);
+  e.preventDefault();
 }
+
+document.body.addEventListener("click", function (e) {
+  const display = new Display();
+  display.deleteBook(e.target);
+  Storage.removeBooks(
+    e.target.parentElement.parentElement.parentElement.firstElementChild
+      .textContent
+  );
+});
